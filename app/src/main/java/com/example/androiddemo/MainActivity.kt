481fd68354a0e27124b8d64cd4d6a3f124929ad4
wongbingg <wb277@naver.com>
2024-06-09 15:51:37 +0900

    feat : 명함 만들기

--- app/src/main/java/com/example/androiddemo/MainActivity.kt
+++ app/src/main/java/com/example/androiddemo/MainActivity.kt
@@ -4,13 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddemo.ui.theme.AndroidDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +44,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    BusinessCard()
+//                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BusinessCard()
                 }
             }
         }
@@ -38,10 +66,78 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun BusinessCard() {
+    val image = painterResource(id = R.drawable.android_logo)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFd3e7d4))
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier
+                    .width(150.dp)
+                    .background(Color(0xFF082f42))
+            )
+            Text(
+                "Wonbeen Lee",
+                fontWeight = FontWeight.W300,
+                fontSize = 40.sp,
+                modifier = Modifier
+                    .padding(10.dp)
+            )
+            Text(
+                "Android Developer Extraordinaire",
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF0b6c38)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(150.dp))
+
+        Column {
+            infoRow(
+                icon = Icons.Filled.Call,
+                content = "010-3856-8436",
+            )
+            infoRow(
+                icon = Icons.Filled.Share,
+                content = "@AndroidDev"
+            )
+            infoRow(
+                icon = Icons.Filled.Email,
+                content = "leewonbeen@gmail.com"
+            )
+        }
+    }
+}
+@Composable
+fun infoRow(icon: ImageVector, content: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            icon,
+            tint = Color(0xFF016d3b),
+            contentDescription = null,
+            modifier = Modifier.padding(10.dp)
+        )
+        Text(content)
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidDemoTheme {
-        Greeting("Android")
+        BusinessCard()
     }
 }
